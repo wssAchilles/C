526c81ff4a083c1df76503baf7c946da652d64c5
Xuziqi <xzqachilles@outlook.com>
2025-03-25 17:13:12 +0800

    Update code for new features

--- cnm/daily life/选择排序法改良.c
+++ cnm/daily life/选择排序法改良.c
@@ -1,26 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int a[10]={0,1,23,434,3,43243,32,11,34,23};
-	int i,j,t;
-	for(i=0;i<9;i++)
+	int a[10] = {0, 1, 23, 434, 3, 43243, 32, 11, 34, 23};
+	int i, j, t;
+	for (i = 0; i < 9; i++)
 	{
-		
-		for(j=i+1;j<10;j++)
+
+		for (j = i + 1; j < 10; j++)
 		{
-			if(a[i]<a[j])
+			if (a[i] > a[j])
 			{
-				t=a[i];
-				a[i]=a[j];
-				a[j]=t;
+				t = a[i];
+				a[i] = a[j];
+				a[j] = t;
 			}
 		}
-		
-		
 	}
-	for(i=0;i<10;i++)
+	for (i = 0; i < 10; i++)
 	{
-		printf("%d,",a[i]);
+		printf("%d,", a[i]);
 	}
 }
-	
